352bdb52c1f838cd7b4fb67c92a6c2765e1fd97e
Waleed Mehmood <80235010+Waleed-Mehmood@users.noreply.github.com>
2025-02-26 11:17:02 +0500

    merge code

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
-<<<<<<< Updated upstream
-=======
 import AboutUs from '../components/AboutUs'
 import PartnerSection from '../components/PartnerSection'
 import Clients from '../components/Clients'
@@ -9,14 +7,11 @@ import Services from '../components/Services'
 import OurProcess from '../components/OurProcess'
 import OurTeam from '../components/OurTeam'
 import Contactsus from '../components/Contactsus'
->>>>>>> Stashed changes
 
 const Home = () => {
   return (
     <div>
       <HeroSection />
-<<<<<<< Updated upstream
-=======
       <AboutUs/>
       <Services/>
       <PartnerSection/>
@@ -24,7 +19,6 @@ const Home = () => {
       <OurProcess/>
       <OurTeam/>
       <Contactsus/>
->>>>>>> Stashed changes
     </div>
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,19 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        scroll: "marquee 15s linear infinite",
+      },
+    },
   },
   plugins: [],
-}
+};
